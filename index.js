cd48d17fb2a24e8c3b8797296fee4f42238bf54b
Diki <adiki436@gmail.com>
2022-10-26 17:15:03 +0700

    tugasexpressjs

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Selamat Datang Di Halaman Utama");
+});
+
+app.get("/hello", (req, res) => {
+  console.log({ urlParams: req.query });
+  res.send("Hello World Dunia!");
+});
+
+app.post("/login", (req, res) => {
+  console.log({ requestFromOutside: req.body });
+  res.send("login berhasil");
+});
+
+app.put("/username", (req, res) => {
+  console.log({ updateData: req.body });
+  res.send("update berhasil");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
